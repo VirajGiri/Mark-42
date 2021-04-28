c31270a6690696a304d08e3b128a8fdc0cb11491
Viraj giri <bhujanggiri2@gmail.com>
2021-04-28 23:40:39 +0530

    added header body card footer component, added 3 mark 42,85 42 cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './body/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
